9cdd29f6f60c6cf09bb2e8ecbb51cd3c500002f9
Anh H. Nguyen <ahn227@lehigh.edu>
2026-05-20 22:44:19 -0400

    Consolidating prod to Render

--- server/src/index.ts
+++ server/src/index.ts
@@ -62,7 +62,7 @@ app.get("/api/health", (_req, res) => {
 if (isProd) {
   const clientDist = path.join(__dirname, "../../client/dist");
   app.use(express.static(clientDist));
-  app.get("*", (_req, res) => {
+  app.get("/{*splat}", (_req, res) => {
     res.sendFile(path.join(clientDist, "index.html"));
   });
 }
